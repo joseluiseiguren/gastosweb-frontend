f04ec6a8c35ea7f73bd8bf24aca4d8d029e2157d
Jose Luis Eiguren <joseluiseiguren@gmail.com>
2018-03-07 20:39:07 +0100

    avances

--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UrlService {
-    //private _host: string = "http://localhost:3000/";
-    private _host: string = "http://controlgastos.eu-4.evennode.com";
+    private _host: string = "http://localhost:3000/";
+    //private _host: string = "http://controlgastos.eu-4.evennode.com";
 
     constructor() { }
 
