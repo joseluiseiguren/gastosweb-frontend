c33b6147879d71c85d67efdcf7dbbe4160be72b6
joseluiseiguren <joseluiseiguren@gmail.com>
2020-02-02 20:35:30 +0100

    xxx

--- src/app/mensual/mensual.component.ts
+++ src/app/mensual/mensual.component.ts
@@ -42,9 +42,12 @@ export const MY_FORMATS = {
 })
 export class MensualComponent implements OnInit, OnDestroy {
   loading: Boolean = false;
+  loadingDetail: Boolean = false;
   conceptosTotales: any[];
+  itemDetail: any[];
   private getDataSubscription: Subscription;
   private summaryDialogSubscription: Subscription;
+  private itemDetailSubscription: Subscription;
   currentDate = new FormControl();
 
   constructor(private _datePipe: DatePipe,
@@ -63,6 +66,7 @@ export class MensualComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.unsubscribeGetData();
     this.unsubscribeSummaryDialog();
+    this.unsubscribeItemDetail();
   }
 
   unsubscribeSummaryDialog(): void {
@@ -71,6 +75,10 @@ export class MensualComponent implements OnInit, OnDestroy {
 
   unsubscribeGetData(): void {
     if (this.getDataSubscription){ this.getDataSubscription.unsubscribe(); }    
+  }
+
+  unsubscribeItemDetail(): void {
+    if (this.itemDetailSubscription){ this.itemDetailSubscription.unsubscribe(); }    
   }
 
   getData() {
@@ -134,135 +142,22 @@ export class MensualComponent implements OnInit, OnDestroy {
     datepicker.close();
     this.getData();
   }
-}
 
-
-/*
-import { Component, OnInit, Inject } from '@angular/core';
-import { DiarioService } from '../services/diario.service';
-import { SumaryMonth } from '../models/sumarymonth';
-import { UsersService } from '../services/users.service';
-import { HelperService } from '../services/helper.service';
-import { SumaryAnio } from '../models/sumaryanio';
-
-@Component({
-  selector: 'app-mensual',
-  templateUrl: './mensual.component.html',
-  styleUrls: ['./mensual.component.css']
-})
-export class MensualComponent implements OnInit {
-  mesActual: Meses;
-  meses: Meses[];
-  anioActual: number = new Date().getFullYear();
-  anios: number[] = new Array<number>();
-  errorMessage: string = "";
-  bsValue: Date;
-  sumMonth: SumaryMonth = new SumaryMonth();
-  sumAnio: SumaryAnio = new SumaryAnio();
-  conceptosTotales: any[];
-  loading: Boolean = false;
-
-  constructor(private _diarioService: DiarioService,
-              private _userService: UsersService,
-              private _helperService: HelperService) { 
-    this.meses = new Array<Meses>();
-    for (let _i = 0; _i < 12; _i++) {
-      this.meses.push(new Meses(this.getMonthName(_i), _i+1));
-    }
-    
-    this.mesActual = this.meses[new Date().getMonth()];
-    this.getPrimerConsumo();
-    this.bsValue = new Date(this.anioActual, this.mesActual.numero-1, 1);
-  }
-
-  ngOnInit() {
-  }
-
-  changeMes(value: number) {
-    let x = new Date(this.bsValue.getFullYear(), value-1, 1);
-    this.bsValue = x;
-    this.mesActual = this.meses[value-1];
-    this.getData();
-  }
-
-  changeAnio(value: number) {
-    let x = new Date(value, this.bsValue.getMonth(), 1);
-    this.bsValue = x;
-    this.anioActual = value;
-    this.getData();
-  }
-
-  private getMonthName(value: number) : string {
-    let x = new Date();
-    x.setMonth(value);
-    let mes = x.toLocaleString(this._appConfig.LOCALE, { month: "long" });
-    mes = mes.charAt(0).toUpperCase() + mes.slice(1).toLowerCase();
-    return mes;
-  }
-
-  getPrimerConsumo() {
-    this.errorMessage = "";
-    this._diarioService.getPrimerConsumo()
+  loadMonthDetails(row: any) {    
+    this.loadingDetail = true;
+    this.itemDetail = undefined;
+    let fecha = this.currentDate.value.getFullYear() + (this.currentDate.value.getMonth() + 1).toString().padStart(2, '0');
+    this.unsubscribeItemDetail();
+    this.itemDetailSubscription = this._diarioService.getConceptosMovimMes(row.idConcepto, fecha)
         .subscribe(
             data => {
-              let anioPrimerConsumo = Number(data.fechaMin.substring(0,4));
-              let anioUltimoConsumo = Number(data.fechaMax.substring(0,4));
-
-              for (let _i = anioUltimoConsumo; _i >= anioPrimerConsumo; _i--) {
-                this.anios.push(_i);
-              }
-              this.getData();
+              this.itemDetail = data;
+              this.loadingDetail = false; 
             },
             error => {
-              this.loading = false; 
-              this.errorMessage = this._helperService.getErrorMessage(error);
+              this.loadingDetail = false; 
+              this.snackBar.open(this._helperService.getErrorMessage(error), '', { duration: 2000, panelClass: ['error-snackbar'], direction: 'ltr', verticalPosition: 'bottom' });
             });
   }
-
-  getData() {
-    this.loading = true;
-    let fecha = this.bsValue.getFullYear().toString() + (this.bsValue.getMonth()+1).toString().padStart(2, '0');
-    this._diarioService.getConceptosTotalMes(fecha)
-        .subscribe(
-            data => { 
-              this.conceptosTotales = data;
-              this.loading = false;
-            },
-            error => {
-              this.loading = false; 
-              this.errorMessage = this._helperService.getErrorMessage(error);
-            });
-  }
-
-  loadDetail(event: boolean, ct: any) {
-    if (event == true) {
-      let fecha = this.bsValue.getFullYear().toString() + (this.bsValue.getMonth()+1).toString().padStart(2, '0');
-      this._diarioService.getConceptosMovimMes(ct.idConcepto, fecha)
-        .subscribe(
-            data => {
-              ct.dataAdic = new Array<any>();
-              ct.dataAdic = data;
-            },
-            error => {
-              this.loading = false; 
-              this.errorMessage = this._helperService.getErrorMessage(error);
-            });
-    }
-  }
-
-  childLoadingStatus(errorMessage: string):void{
-    this.errorMessage = errorMessage;
-  }
-
 }
 
-class Meses {
-  nombre: string;
-  numero: number;
-
-  constructor(nombre: string, numero: number) { 
-    this.nombre = nombre;
-    this.numero = numero;
-  }
-}
-*/
